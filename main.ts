38aa955cd43ec55ac56447da3ba242d99ba1673d
Bones <58165029+Bons18@users.noreply.github.com>
2026-05-22 11:43:56 -0500

    Update main.ts
    Server

--- main.ts
+++ main.ts
@@ -1,5 +1,21 @@
-import { serveFile } from "jsr:@std/http/file-server";
+Deno.serve((req) => {
 
-Deno.serve((req: Request) => {
-    return serveFile(req, "./index.html");
+  const url = new URL(req.url);
+
+  if (url.pathname === "/fileinfo") {
+    return new Response("FILEINFO_OK");
+  }
+
+  if (url.pathname === "/x-shader") {
+    return new Response("SHADER_OK");
+  }
+
+  if (url.pathname === "/assetindexer-v2") {
+    return new Response("ASSET_OK");
+  }
+
+  return new Response("404", {
+    status: 404
+  });
+
 });

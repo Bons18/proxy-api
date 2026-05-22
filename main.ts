d88577a9ab4b24fe9724a190ad9043b72ec27f45
Bones <58165029+Bons18@users.noreply.github.com>
2026-05-22 12:18:50 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,14 +2,22 @@ Deno.serve((req) => {
 
   const url = new URL(req.url);
 
+  // HOME
+  if (url.pathname === "/") {
+    return new Response("SERVER_ONLINE");
+  }
+
+  // FILEINFO
   if (url.pathname === "/fileinfo") {
     return new Response("FILEINFO_OK");
   }
 
+  // SHADER
   if (url.pathname === "/x-shader") {
     return new Response("SHADER_OK");
   }
 
+  // ASSET
   if (url.pathname === "/assetindexer-v2") {
     return new Response("ASSET_OK");
   }
